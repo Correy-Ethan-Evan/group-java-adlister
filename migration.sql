08c990c5951c2b0c66c962b3696868813bddfbd2
Evan Mead <evan.c.mead@gmail.com>
2021-02-03 10:18:14 -0600

    migration.sql updated

--- migration.sql
+++ migration.sql
@@ -1,22 +1,38 @@
 USE adlister_db;
 
-DROP TABLE IF EXISTS ads;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS bondsman;
+DROP TABLE IF EXISTS bounty;
+DROP TABLE IF EXISTS bounty_hunter;
 
-CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE user
+(
+    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
+    email    VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    role_id  INT(10) UNSIGNED,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    UNIQUE (username)
 );
 
-CREATE TABLE ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
+CREATE TABLE bounty
+(
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    bounty_id   INT UNSIGNED NOT NULL,
+    title       VARCHAR(240) NOT NULL,
+    description TEXT         NOT NULL,
+    priority    INT          NOT NULL,
+    risk        VARCHAR(10)  NOT NULL,
+    payout      INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (bounty_id) REFERENCES users (id)
         ON DELETE CASCADE
 );
+
+CREATE TABLE role
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+)
